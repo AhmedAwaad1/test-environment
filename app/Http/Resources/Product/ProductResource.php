0af84cbe18c25953d9007e632b6949adcfbb9c19
basselraouf <bassel.raouf@gmail.com>
2025-05-20 16:33:22 +0300

    restructure product variant and add new table for variant sizes

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -3,9 +3,11 @@
 namespace App\Http\Resources\Product;
 
 use App\Http\Resources\Category\CategoryResource;
+use App\Http\Resources\Color\ColorResource;
 use App\Http\Resources\ProductImage\ProductImageResource;
 use App\Http\Resources\ProductType\ProductTypeResource;
 use App\Http\Resources\ProductVariant\ProductVariantResource;
+use App\Http\Resources\Size\SizeResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,6 +20,7 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
         return [
             "id" => $this->id,
             'name_en' => $this->name_en,
@@ -26,6 +29,21 @@ class ProductResource extends JsonResource
             'description_ar' => $this->description_ar,
             'price' => (int) $this->price,
             'price_after_discount' => (int) $this->price_after_discount,
+            'sizes' => SizeResource::collection(
+                collect($this->productVariants)
+                    ->pluck('variantSizes') // لو sizes جاية من علاقة variantSizes
+                    ->flatten()
+                    ->pluck('size')
+                    ->unique('id')
+                    ->values()
+            ),
+
+            'colors' => ColorResource::collection(
+                collect($this->productVariants)
+                    ->pluck('color')
+                    ->unique('id')
+                    ->values()
+            ),
             'category' => new CategoryResource($this->whenLoaded('category')),
             'product_type' => new ProductTypeResource($this->whenLoaded('productType')),
             'product_images' => ProductImageResource::collection($this->whenLoaded('images')),
