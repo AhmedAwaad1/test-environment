6136a5cd102b4a6fa6f8da16a0169bfb36fcc316
basselraouf <bassel.raouf@gmail.com>
2025-05-29 11:13:03 +0300

    variant managment and variant opertaions end points

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             ColorSeeder::class,
             CategorySeeder::class,
             SubCategorySeeder::class,
-            // ProductSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
